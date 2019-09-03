415934bc8ae20b236acff281d72c9664f6fdf5fd
ChidiChuks <chidiebere_chukwuma@yahoo.com>
2019-09-03 12:57:00 +0100

    creating the subscription pop-up box

--- README.md
+++ README.md
@@ -2,4 +2,7 @@
 Using `Service Workers` to create a Push Notification on a NodeJs App
 
 ## Installing web dependencies
-npm i web-push express body-parser
+- npm i web-push express body-parser
+
+## To create the vapid keys 
+- you need to direct your path towards the created node-modules file `E:\PROJECTS\PushNotify\pushNotify\node_modules\.bin/web-push generate-vapid-keys`
--- index.js
+++ index.js
@@ -1,4 +1,43 @@
 const express = require('express');
+require('dotenv').config();
 const webpush = require('web-push');
 const bodyParser = require('body-parser');
 const path = require('path');
+
+// initializing express
+const app = express();
+
+// configure static folder where client stuffs will be placed (Set static path)
+/* Current directory means = __dirname*/
+app.use(express.static(path.join(__dirname, "client")));
+
+// adding the body-parser middleware
+app.use(bodyParser.json());
+
+/* Vapid Keys- Identifies the person sending out the notifications */
+// create a set of vapid keys
+const publicVapidKey = process.env.Public_Key;
+const privateVapidKey = process.env.Private_Key;
+
+// setting vapid details
+webpush.setVapidDetails('mailto:test@test.com', publicVapidKey, privateVapidKey);
+
+// Subscribe Route
+app.post('/subscribe', (req, res) => {
+    // Get pushSubscription object
+    const subscription = req.body;
+
+    // Send 201 - resource created
+    res.status(201).json({});
+
+    // Create Payload
+    const payload = JSON.stringify({ title: 'Push Test' });
+
+    // Pass object into send Notification
+    webpush.sendNotification(subscription, payload).catch(err => console.error(err));
+});
+
+const port = process.env.PORT || 5000;
+
+// listen on the port
+app.listen(port, () => console.log(`Server started on port ${port}`));
